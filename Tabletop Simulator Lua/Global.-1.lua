e794ba8e2deaa0568ccf1f446241c64efedec4c5
maxm4004 <maxm4004@gmail.com>
2026-04-22 16:51:37 +0200

    v1.35.10
    v1.35.10

--- Tabletop Simulator Lua/Global.-1.lua
+++ Tabletop Simulator Lua/Global.-1.lua
@@ -1,11 +1,54 @@
---[[ Lua code. See documentation: https://api.tabletopsimulator.com/ --]]
+VERSION = "v1.35.10"
+ENGINE = nil
+DEBUG = false
+army1 = "Army1"
+army2 = "Army2"
 
---[[ The onLoad event is called after the game save finishes loading. --]]
-function onLoad()
-    --[[ print('onLoad!') --]]
-end
+require("common.constants")
+require("common.utils")
+require("engine.core")
+require("engine.conditions")
+require("engine.input")
+require("engine.ui")
+require("rulesets.lionheart.callbacks")
+require("rulesets.lionheart.phases")
+require("rulesets.lionheart.persistence")
 
---[[ The onUpdate event is called once per frame. --]]
-function onUpdate()
-    --[[ print('onUpdate loop!') --]]
-end
+colorArmy1 = COLOR.NOARMY
+colorArmy2 = COLOR.NOARMY
+        
+
+
+-- ============================================================
+-- FUNZIONE: onLoad()
+-- ============================================================
+    function onLoad(save_state)
+
+        clear()
+        lionheart_initPhases()
+
+        if save_state and save_state ~= "" then
+            ripristinaStato(save_state)
+        end
+
+        engine_init()
+
+        if save_state and save_state ~= "" then
+            Wait.time(function()
+                ripristinaColoriPlayers()
+                updateCenterPanel()
+            end, 1)
+        end
+    end
+-- ============================================================
+-- FUNZIONE: onSave()
+-- ============================================================
+    function onSave()     
+        return lionheart_onSave()
+    end
+-- ============================================================
+-- FUNZIONE: onChat()
+-- ============================================================
+    function onChat(message, player)
+        return engine_handleChat(message, player)
+    end
